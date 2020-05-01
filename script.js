79c491f84b2156f24b07fd9fa1725a6f31d4e5ec
hajime hatakeyama <63620706+hajime-hatakeyama@users.noreply.github.com>
2020-05-01 10:51:28 +0900

    omikuji

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
-$(function () {
-  const $change = $("#change-css");
-  const $text = $("#text");
+"use strict";
 
-  $change.click(function () {
-    document.getElementById("papa2").src = "img/20200419hanu2.PNG";
-    document.getElementById("mama2").src = "img/20200419sei2.PNG";
-    document.getElementById("meru2").src = "img/20200419meru5.PNG";
-    document.getElementById("runa2").src = "img/runaIntro2.PNG";
-    // document.getElementById("text").href = "http://google.co.jp";
-    $change.html("登録済み");
-    // document.getElementById($text).innerHTML(
-    //   <h1>
-    //     <a href="movie/runa20200422.mp4">"ようこそ！畠山わんこ一家へ!")</a>
-    //   </h1>
-    // );
-    $text.html("ボタンを押した後<br>もう一度写真を見てみて!");
-    $text.css("color", "red");
-    $text.css("font-size", "30px");
-    $text.css("text-decoration", "underline");
-    // document.getElementById("text").href = "http://google.co.jp";
-    // $text.fadeOut(2000);
-  });
+{
+  const btn = document.getElementById("btn");
 
-  $(function () {
-    $(".lesson-icon").hover(
-      function () {
-        $(".txt-contents").fadeIn();
-      },
-      function () {
-        $(".txt-contents").fadeOut();
-      }
-    );
-  });
-});
+  btn.addEventListener(
+    "click",
+    () => {
+      const results = ["大吉", "中吉", "小吉", "凶"];
+      // const n = Math.floor(Math.random() * results.length);
+      // btn.textContent = n;
+      btn.textContent = results[Math.floor(Math.random() * results.length)];
+      // const n = Math.random();
+      // if (n < 0.05) {
+      //   btn.textContent = "大吉";
+      // } else if (n < 0.2) {
+      //   btn.textContent = "中吉";
+      // } else {
+      //   btn.textContent = "凶";
+      // }
+    }
+
+    // switch (n) {
+    //   case 0:
+    //     btn.textContent = "大吉";
+    //     break;
+    //   case 1:
+    //     btn.textContent = "中吉";
+    //     break;
+    //   case 2:
+    //     btn.textContent = "凶";
+    //     break;
+    // }
+  );
+}
